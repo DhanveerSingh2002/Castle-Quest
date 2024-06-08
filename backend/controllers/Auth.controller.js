f87d7f078eaf36c17540e245562f8bb316476733
DhanveerSingh2002 <100077027+DhanveerSingh2002@users.noreply.github.com>
2024-06-08 19:11:12 +0530

    Sign Up page completed 100%

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -1,9 +1,14 @@
 const User = require("../models/User.model");
+const bcryptjs = require('bcryptjs');
 
 const signUp = async (req, res, next) => 
 {
     const {username, email, password} = req.body;
-    const newUser = new User({username, email, password});
+    if (!password || password.length === 0) {
+        return res.status(400).json({ error: "Password is required and cannot be empty." });
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({username, email, password: hashedPassword});
     try {
         await newUser.save();
         res.status(201).json(newUser);
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,21 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/sign-up", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      //console.log(data);
+      if (data.success === "false") {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-center text-4xl my-10 text-teal-400'>Sign Up</h1>
-      <form className='flex flex-col justify-center gap-5 text-teal-300'>
-        <input className='bg-[#080F22] bg-transparent focus:outline-none rounded-lg p-3' type="text" id='username' placeholder='Username'/>
-        <input className='bg-[#080F22] bg-transparent focus:outline-none p-3 rounded-lg' type="email" id='email' placeholder='Email'/>
-        <input className='bg-[#080F22] bg-transparent focus:outline-none rounded-lg p-3' type="password" id='password' placeholder='Password'/>
-        <button className='border-transparent bg-slate-800 rounded-lg hover:bg-teal-500 hover:text-black hover:scale-105 duration-300 transition-all ease-in-out bg-opacity-50 p-3'>Register</button>
+      <form className='flex flex-col justify-center gap-5 text-teal-300' onSubmit={handleSubmit}>
+        <input className='bg-[#080F21] focus:outline-none rounded-lg p-3' type="text" id='username' placeholder='Username' onChange={handleChange} />
+        <input className='bg-[#080F22] focus:outline-none p-3 rounded-lg' type="email" id='email' placeholder='Email' onChange={handleChange} />
+        <input className='bg-[#080F22] focus:outline-none rounded-lg p-3' type="password" id='password' placeholder='Password' onChange={handleChange} />
+        <button disabled={loading} className='border-transparent bg-slate-800 rounded-lg hover:bg-teal-500 hover:text-black hover:scale-105 duration-300 transition-all ease-in-out bg-opacity-50 p-3'>{loading ? "Please Wait..." : "Register"}</button>
         <button className='border-transparent bg-teal-950 rounded-lg hover:bg-teal-500 hover:text-black hover:scale-105 duration-300 transition-all ease-in-out bg-opacity-50 p-3'>Continue with Google</button>
       </form>
       <div className='flex gap-2 mt-4'>
-        <span>Already having an Account?</span>
-        <Link to="/sign-in"><span className='text-teal-300 opacity-80 hover:opacity-100'>Sign in</span></Link>
+        <p>Already having an Account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-teal-300 opacity-80 hover:opacity-100'>Sign in</span>
+        </Link>
       </div>
+      {error && <p className='text-teal-400 mt-5'>{error}</p>}
     </div>
   )
 }
