a598488aa2e56a16a32963b6294563b96b87e7dd
DhanveerSingh2002 <100077027+DhanveerSingh2002@users.noreply.github.com>
2024-06-16 13:56:49 +0530

    Completed profile page interface

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
             <Link  to="/profile">
                 {
                   currentUser? 
-                  <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile'/>:
+                  <img className='rounded-full h-7 w-7 object-cover mr-4 sm:mr-0' src={currentUser.avatar} alt='profile'/>:
                   <li className='cursor-pointer text-teal-300 transition-all duration-500 hover:scale-125 opacity-80 hover:opacity-100 mr-2 sm:mr-0'>Sign in</li>
                 }
             </Link>
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const Profile = () => {
+  const {currentUser} = useSelector((state) => state.user)
   return (
-    <div>
-      are
+    <div className="max-w-lg mx-auto p-2 sm:p-0">
+      <h1 className='text-center text-4xl font-semibold text-teal-400 mt-8 mb-6'>Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img className="h-28 w-28 rounded-full object-cover cursor-pointer my-4 self-center" src={currentUser.avatar} alt="Profile Photo"/>
+        <input className='bg-[#080F21] text-teal-400 focus:outline-none rounded-lg p-3' type="text" id='username' placeholder='Username' />
+        <input className='bg-[#080F22] text-teal-400 focus:outline-none p-3 rounded-lg' type="email" id='email' placeholder='Email' />
+        <input className='bg-[#080F22] text-teal-400 focus:outline-none rounded-lg p-3' type="password" id='password' placeholder='Password' />
+        <button className="border-transparent text-teal-400 text-xl bg-cyan-950 rounded-lg hover:bg-teal-500 hover:text-black hover:scale-105 duration-300 transition-all ease-in-out bg-opacity-50 p-3">Edit &nbsp;✎</button>
+      </form>
+      <div className="flex mt-3 justify-between">
+        <span className="text-red-500 cursor-pointer hover:underline transition-all duration-500">Delete Account</span>
+        <span className="text-red-500 cursor-pointer hover:underline transition-all duration-500">Sign out</span>
+      </div>
     </div>
   )
 }
