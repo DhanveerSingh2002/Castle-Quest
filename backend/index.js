19b6a5115fb37c7be7f04721f2999f5a5300ecbd
DhanveerSingh2002 <100077027+DhanveerSingh2002@users.noreply.github.com>
2024-07-12 11:27:13 +0530

    Added the pre-installation for deployment

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const userRouter = require('./routes/User.route');
 const authRouter = require('./routes/Auth.route');
 const listingRouter = require('./routes/listing.route');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 
+const __directoryName = path.resolve();
+
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 })
@@ -26,6 +29,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__directoryName, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__directoryName, 'frontend','dist','index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
