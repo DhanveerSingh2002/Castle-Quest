374385a96ff032de589200b9fe5682777067b4fd
DhanveerSingh2002 <100077027+DhanveerSingh2002@users.noreply.github.com>
2024-07-12 14:24:55 +0530

    Cookie ccess corrected

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -34,7 +34,7 @@ const signIn = async (req, res, next) => {
         }
         const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
         const {password:pass, ...rest} = validUser._doc;
-        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+        res.cookie('access_token', token, { httpOnly: true, maxAge: 365*24*60*60*1000 }).status(200).json(rest);
     } catch (error) {
         next(error);
     }
